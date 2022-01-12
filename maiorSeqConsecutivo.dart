2c76f6d0d2a3b9a09cfc0d48fbd0a9cc9d2dec79
Renato Leal <marcondesleal@icloud.com>
2022-01-12 13:44:36 -0300

    Create maiorSeqConsecutivo.dart
    1º Commit do projeto.

---
+++ maiorSeqConsecutivo.dart
@@ -0,0 +1,44 @@
+void main() {
+  final String numeroInformado = 1267378901.toString();
+  obterMaxSequencia(numeroInformado);
+}
+
+obterMaxSequencia(String numInfo) {
+  List<String> convertStringEmListString = numInfo.split("");
+  List<int> listInt = [];
+  for (var element in convertStringEmListString) {
+    listInt.add(int.parse(element));
+  }
+  List resultadoFinal = [];
+  List<dynamic> resultadoTempInt = [];
+  List<String> resultadoTempString = [];
+  var strTemp = null;
+
+  listInt.forEach((element) {
+    if (resultadoTempInt.length == 0) {
+      resultadoTempInt.add(element);
+    } else {
+      if ((element == resultadoTempInt.last + 1) ||
+          (element == 0 && resultadoTempInt.last == 9)) {
+        resultadoTempInt.add(element);
+      } else {
+        for (var elemento in resultadoTempInt) {
+          resultadoTempString.add(elemento.toString());
+        }
+        strTemp = resultadoTempString.join();
+        resultadoFinal.add(strTemp);
+        resultadoTempInt.clear();
+        resultadoTempString.clear();
+        resultadoTempInt.add(element);
+      }
+    }
+  });
+  if (resultadoTempInt.length != 0) {
+    for (var elementos in resultadoTempInt) {
+      resultadoTempString.add(elementos.toString());
+    }
+    strTemp = resultadoTempString.join();
+    resultadoFinal.add(strTemp);
+  }
+  print(resultadoFinal);
+}
